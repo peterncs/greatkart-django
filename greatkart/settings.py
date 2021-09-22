2baf1f9419a6e92553609eaa6cf7f66d7d29ada9
Sing <peterncs97@gmail.com>
2021-09-22 08:52:08 +0800

    ams deployment started and allowed host

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['greatkart-course-env.eba-psxirtpm.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
